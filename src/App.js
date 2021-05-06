7d89621eb6ab78a492e90b09fc80edc2357979d2
Guilherme <guilhermesanm2003@gmail.com>
2021-05-06 12:21:13 -0300

    shuffle function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 import './App.css';
 import { cards } from './tarot.json';
 
 const baseUrl = "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341"
 
-export default App;
-
 function App() {
+  
+  const [cartas, setCartas] = useState(cards)
+
+  const shuffle = (cards) => {
+    // funcao embaralhar
+    
+    for (let i = cards.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * (i + 1));
+      [cards[i], cards[j]] = [cards[j], cards[i]];
+    };
+    setCartas(cards);
+  }
+
   return (
     <div className="App">
-   
-          {cards.map(cards => (
-            <li key={cards.image}> <img src= {`${baseUrl}/${cards.image}`} /></li>
-            ))}
-        
+        <h1> Personare </h1>
+
+          <div className="card">
+            {cartas.map(cards => (
+              <li key={cards.image}> <img src= {`${baseUrl}/${cards.image}`} alt="card" className="middle"/></li>
+              ))}
+          </div>
+
+          <button onClick={() => {shuffle(cards)}}> Embaralhar </button>
+
     </div>
   );
 }
 
+
+export default App;
