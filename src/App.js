3a830207f77fe3a4bc71d84c5822d98518f0b668
Guilherme <guilhermesanm2003@gmail.com>
2021-05-06 21:55:50 -0300

    quase final

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React, { useState } from "react";
 import './App.css';
 import { cards } from './tarot.json';
+import swal from 'sweetalert';
+
 
 const baseUrl = "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341"
+const back = 'https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png'
 
 function App() {
   
-  const [cartas, setCartas] = useState(cards)
+  const [cartas, setCartas] = useState(cards);
+  const [viradas, setViradas] = useState(false);
 
-  const shuffle = (cards) => {
-    // funcao embaralhar
-    
-    for (let i = cards.length - 1; i > 0; i--){
-      const j = Math.floor(Math.random() * (i + 1));
-      [cards[i], cards[j]] = [cards[j], cards[i]];
-    };
-    setCartas(cards);
+  const shuffle = (array) => { 
+     setCartas([...array.sort(() => 0.5 - Math.random())]);
   }
 
   return (
     <div className="App">
         <h1> Personare </h1>
-
+          <button onClick={() => shuffle(cartas)}> Embaralhar </button>
+          <button onClick={() => setViradas(!viradas)}> Virar </button>
+          
           <div className="card">
-            {cartas.map(cards => (
-              <li key={cards.image}> <img src= {`${baseUrl}/${cards.image}`} alt="card" className="middle"/></li>
+            {cartas.map(card => (
+              <li onClick={() => swal(card.name) } key={ card.image }> <img src= { !viradas ? back : `${ baseUrl }/${ card.image }`} alt="card" className="middle"/></li>
               ))}
           </div>
-
-          <button onClick={() => {shuffle(cards)}}> Embaralhar </button>
 
     </div>
   );
